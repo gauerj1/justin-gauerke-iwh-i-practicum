b13844528959457451dbbe9e9757a13dbb29bf23
gauerj1 <justin@meta13.com>
2024-12-31 20:47:59 +0000

    completed project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -9,8 +11,9 @@ app.use(express.json());
 
 
 
+
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = 'pat-na1-7019266b-b306-4145-80db-9d559205eb83';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 //JPG Updates
