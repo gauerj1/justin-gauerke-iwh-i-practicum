110d83104e97aab7fe3365ac18dd226e105e8596
gauerj1 <justin@meta13.com>
2024-12-31 21:03:14 +0000

    Cleanup

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
